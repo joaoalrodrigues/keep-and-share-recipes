36736a420153868665ca8132423b67c754f41dae
João Rodrigues <jalxnd@gmail.com>
2025-01-16 15:36:56 +0100

    refactor: make `npm test` more robust with `async-retry` and `orchestrator.js`

---
+++ tests/orchestrator.js
@@ -0,0 +1,27 @@
+import retry from "async-retry";
+import database from "infra/database";
+
+async function waitForAllServices() {
+  await waitForWebServer();
+
+  async function waitForWebServer() {
+    return retry(fetchStatusPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
+
+    async function fetchStatusPage() {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      const responseBody = await response.json();
+    }
+  }
+}
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+export default {
+  waitForAllServices,
+  cleanDatabase,
+};
