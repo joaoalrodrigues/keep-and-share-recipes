d422d2769aed311a102d4be47e88649c2a9c9b7b
João Rodrigues <jalxnd@gmail.com>
2025-03-04 16:35:11 +0100

    feat: add `runPendingMigrations` method to orchestrator

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,8 +1,6 @@
-import { resolve } from "path";
-import migrationRunner from "node-pg-migrate";
-import database from "/infra/database";
 import { createRouter } from "next-connect";
 import controller from "infra/controller";
+import migrator from "models/migrator";
 
 const router = createRouter();
 
@@ -11,36 +9,14 @@ router.post(postHandler);
 
 export default router.handler(controller.errorHandlers);
 
-const defaultMigrationOptions = {
-  databaseUrl: process.env.DATABASE_URL,
-  dryRun: true,
-  dir: resolve("infra", "migrations"),
-  direction: "up",
-  verbose: true,
-  migrationsTable: "pgmigrations",
-};
-
 async function getHandler(_, response) {
-  const dbClient = await database.getNewClient();
-  const pendingMigrations = await migrationRunner({
-    ...defaultMigrationOptions,
-    dbClient,
-  });
-
-  dbClient.end();
+  const pendingMigrations = await migrator.listPendingMigrations();
 
   response.status(200).json(pendingMigrations);
 }
 
 async function postHandler(_, response) {
-  const dbClient = await database.getNewClient();
-  const migratedMigrations = await migrationRunner({
-    ...defaultMigrationOptions,
-    dbClient,
-    dryRun: false,
-  });
-
-  dbClient.end();
+  const migratedMigrations = await migrator.runPendingMigrations();
 
   if (migratedMigrations.length > 0) {
     return response.status(201).json(migratedMigrations);
